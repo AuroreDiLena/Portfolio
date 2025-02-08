438c39341c603ee63427427256f8e5d151da663f
Aurore Di Lena <dilena.aurore05@gmail.com>
2025-02-08 15:08:31 +0100

    pas voir moins dans projet

--- Javascript/ScriptTooglePopup.js
+++ Javascript/ScriptTooglePopup.js
@@ -2,6 +2,5 @@ document.querySelectorAll('.toggle-btn').forEach(button => {
     button.addEventListener('click', () => {
         const content = button.previousElementSibling;
         content.classList.toggle('active');
-        button.textContent = content.classList.contains('active') ? 'Voir moins' : 'Voir plus';
     });
 });
